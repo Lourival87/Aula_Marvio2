35cecac9aacad1bc731ef0d053bc2811e8290f62
Lourival87 <102527848+Lourival87@users.noreply.github.com>
2022-05-12 21:20:18 -0300

    Add files via upload

---
+++ Código_2.js
@@ -0,0 +1,59 @@
+// Initialize Parse
+Parse.initialize("JfMeozLs8UZFxaZibAiZhlpDl5OZkyjVwzdxLfqw", "fi6LWURzRGmTg7neZfI79MJaB2QHjWhiZ4nVFvKD");
+Parse.serverURL = "https://parseapi.back4app.com/";
+   
+
+
+const inputElement = document.getElementById("tex");
+const buttonElement = document.getElementById("btn");
+const ulElement = document.getElementById("lis");
+const SelectElement = document.getElementById("op")
+
+
+
+let tarefas = [];
+
+buttonElement.onclick = ev => {
+  ev.preventDefault();
+
+
+  if (inputElement.value) {
+    
+    
+    const textElement = document.createElement("span");
+    textElement.innerHTML = inputElement.value;
+
+    const btnElement = document.createElement("button");
+    btnElement.innerHTML = "Remover";
+
+    const btncon = document.createElement('input');
+    btncon.type = "checkbox";
+    btncon.id = "check";
+    
+    
+
+    const liElement = document.createElement("li");
+    liElement.appendChild(textElement);
+    liElement.appendChild(btnElement);
+    liElement.appendChild(btncon);
+    
+
+    btnElement.onclick = () => {
+      ulElement.removeChild(liElement)  
+    }
+    
+    btncon.onclick = () => {
+        textElement.style.textDecoration = 'line-through';
+    }
+    
+  
+ 
+
+ulElement.appendChild(liElement);
+    inputElement.value = "";
+
+    
+  
+   }
+    
+  }
